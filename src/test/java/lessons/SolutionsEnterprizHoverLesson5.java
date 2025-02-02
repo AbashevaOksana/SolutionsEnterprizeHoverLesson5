35a8bd76d4dd01110b43fce3fae600210e322111
Abasheva Oksana <antona9@ya.ru>
2025-02-03 02:24:48 +0400

    added java - test

--- src/test/java/lessons/SolutionsEnterprizHoverLesson5.java
+++ src/test/java/lessons/SolutionsEnterprizHoverLesson5.java
@@ -1,9 +1,13 @@
 package lessons;
 
+import com.codeborne.selenide.CollectionCondition;
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.DragAndDropOptions;
+import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.CollectionCondition.*;
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
@@ -11,22 +15,40 @@ import static com.codeborne.selenide.Selenide.*;
 public class SolutionsEnterprizHoverLesson5 {
     @BeforeAll
     static void beforeAll() {
-        Configuration.baseUrl = "https://github.com";
+        //Configuration.baseUrl = "https://github.com";
         Configuration.browserSize = "1920x1080";
         Configuration.pageLoadStrategy = "eager";
-        Configuration.holdBrowserOpen = false;
+        Configuration.holdBrowserOpen = true;
         Configuration.timeout = 10000;
     }
 
     @Test
-    void runRun () {
-        open("");
+    void hoverTest () {
+        open("https://github.com");
         $(".HeaderMenu-nav").$(byText("Solutions")).hover();
         $("[href='https://github.com/enterprise']").click();
-        $("#hero-section-brand-heading").shouldHave(text("The AI-powered"));
-        $("#hero-section-brand-heading").shouldHave(text("The AI-powered"));
+        $("#hero-section-brand-heading").shouldHave(text("""
+                The AI-powered
+                developer platform"""));
+
 
     }
     @Test
-    void
+    void dragAndDropWithActionsTest () {
+        open("https://the-internet.herokuapp.com/drag_and_drop");
+        $("#column-a").shouldHave(text("A")).hover();
+        actions().clickAndHold().moveToElement($("#column-b")).release().perform();
+        $("#column-a").shouldHave(text("B"));
+        $("#column-b").shouldHave(text("A"));
+
+    }
+
+    @Test
+    void dragAndDropWithOptionsTest () {
+        open("https://the-internet.herokuapp.com/drag_and_drop");
+        $("#column-a").shouldHave(text("A")).hover();
+        $("#column-a").dragAndDrop(DragAndDropOptions.to($("#column-b")));
+        $("#column-a").shouldHave(text("B"));
+        $("#column-b").shouldHave(text("A"));
+    }
 }
